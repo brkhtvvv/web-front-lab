ca2ea1d5a9b7f40a82d402eb60094abea08e9044
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-05-31 20:52:28 +0700

    вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -14,3 +14,21 @@ function showDate() {
     outJp.innerHTML = today.toLocaleString('ja-JP');
     outEs.innerHTML = today.toLocaleString('es-ES');
 }
+function showDateTime() {
+    let currentDate = new Date();
+    let dateTimeOutput = document.getElementById('current-date-time');
+    let yearOutput = document.getElementById('current-year');
+    let monthOutput = document.getElementById('current-month');
+    let dayOutput = document.getElementById('current-day');
+    let dayOfWeekOutput = document.getElementById('current-day-of-week');
+
+    dateTimeOutput.textContent = currentDate.toLocaleString();
+    yearOutput.textContent = currentDate.getFullYear();
+    monthOutput.textContent = currentDate.getMonth() + 1; // Месяцы в JavaScript начинаются с 0
+    dayOutput.textContent = currentDate.getDate();
+    dayOfWeekOutput.textContent = getDayOfWeekName(currentDate.getDay());
+}
+function getDayOfWeekName(dayIndex) {
+    const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    return daysOfWeek[dayIndex];
+}
