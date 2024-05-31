ac81d33dd5d0d8ae3df0f062eea1fbb8cc463e5f
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-05-31 20:45:53 +0700

    вывод даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -14,35 +14,3 @@ function showDate() {
     outJp.innerHTML = today.toLocaleString('ja-JP');
     outEs.innerHTML = today.toLocaleString('es-ES');
 }
-
-function showDateTime() {
-    let currentDate = new Date();
-    let dateTimeOutput = document.getElementById('current-date-time');
-    let yearOutput = document.getElementById('current-year');
-    let monthOutput = document.getElementById('current-month');
-    let dayOutput = document.getElementById('current-day');
-    let dayOfWeekOutput = document.getElementById('current-day-of-week');
-
-    dateTimeOutput.textContent = currentDate.toLocaleString();
-    yearOutput.textContent = currentDate.getFullYear();
-    monthOutput.textContent = currentDate.getMonth() + 1; // Месяцы в JavaScript начинаются с 0
-    dayOutput.textContent = currentDate.getDate();
-    dayOfWeekOutput.textContent = getDayOfWeekName(currentDate.getDay());
-}
-
-function getDayOfWeekName(dayIndex) {
-    const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    return daysOfWeek[dayIndex];
-}
-
-function getDayOfWeek() {
-    const inputDate = document.getElementById('input-date').value;
-    const inputMonth = document.getElementById('input-month').value - 1; // Месяцы в JavaScript начинаются с 0
-    const inputYear = document.getElementById('input-year').value;
-
-    const inputDateObj = new Date(inputYear, inputMonth, inputDate);
-    const dayOfWeek = getDayOfWeekName(inputDateObj.getDay());
-
-    const outputDiv = document.getElementById('output-day-of-week');
-    outputDiv.textContent = `День недели для указанной даты: ${dayOfWeek}`;
-}
